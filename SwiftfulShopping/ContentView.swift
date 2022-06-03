4ae863148e81d608f1ade494f956e51d699a4956
Łukasz Janiszewski <ljaniszewski00@gmail.com>
2022-06-03 18:30:13 +0200

    Initial Commit

---
+++ SwiftfulShopping/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftfulShopping
+//
+//  Created by Łukasz Janiszewski on 03/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftfulShopping/SwiftfulShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulShoppingApp.swift
+//  SwiftfulShopping
+//
+//  Created by Łukasz Janiszewski on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
