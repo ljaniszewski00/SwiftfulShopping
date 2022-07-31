24b4a83957c5d0b0a911abfb2b0f3b0d6b4a6074
Łukasz Janiszewski <ljaniszewski00@gmail.com>
2022-07-31 14:48:33 +0200

    Fixed OrderDetailsView buttons to be able to rate the order

--- SwiftfulShopping/Home/Tabs/Profile/Orders/OrderDetailsView.swift
+++ SwiftfulShopping/Home/Tabs/Profile/Orders/OrderDetailsView.swift
@@ -17,109 +17,115 @@ struct OrderDetailsView: View {
     var order: Order
     
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            VStack(alignment: .leading, spacing: 40) {
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("Order Date")
-                        .font(.system(size: 18, design: .rounded))
-                    Text(Date.getDayMonthYearFrom(date: order.orderDate))
-                        .font(.system(size: 20, weight: .bold, design: .rounded))
-                        .foregroundColor(.accentColor)
-                }
-                
-                HStack(alignment: .top) {
-                    VStack(alignment: .leading, spacing: 15) {
-                        Text("Customer Info")
-                            .font(.system(size: 18, weight: .medium, design: .rounded))
-                            .foregroundColor(.accentColor)
-                        Text(order.client.description)
-                            .font(.system(size: 14, design: .rounded))
-                            
-                    }
-                    Spacer()
-                    VStack(alignment: .leading, spacing: 15) {
-                        Text("Shipping Info")
-                            .font(.system(size: 18, weight: .medium, design: .rounded))
-                            .foregroundColor(.accentColor)
-                        Text(order.shippingAddress.description)
-                            .font(.system(size: 14, design: .rounded))
-                            
-                    }
-                }
-                .padding(.bottom, 20)
-                
-                HStack(alignment: .bottom) {
+        VStack {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .leading, spacing: 40) {
                     VStack(alignment: .leading, spacing: 10) {
-                        Text("Status")
+                        Text("Order Date")
                             .font(.system(size: 18, design: .rounded))
-                            
-                        Text(order.status.rawValue)
+                        Text(Date.getDayMonthYearFrom(date: order.orderDate))
                             .font(.system(size: 20, weight: .bold, design: .rounded))
                             .foregroundColor(.accentColor)
                     }
-                    .padding(.bottom, 10)
                     
-                    Spacer()
-                    
-                    Button(action: {
-                        showProductsList.toggle()
-                    }, label: {
-                        Image(systemName: showProductsList ? "chevron.up" : "chevron.down")
-                    })
-                    .padding(.trailing)
-                }
-                
-                if showProductsList {
-                    ForEach(Array(order.shoppingCart.products.keys), id: \.self) { product in
-                        HStack(alignment: .top) {
-                            Image("product_placeholder_image")
-                                .resizable()
-                                .frame(width: 150, height: 150)
-                            VStack(alignment: .leading, spacing: 20) {
-                                Text(product.id)
-                                    .font(.system(size: 14, design: .rounded))
-                                HStack(alignment: .bottom) {
-                                    VStack(alignment: .leading, spacing: 5) {
-                                        Text(product.name)
-                                            .font(.system(size: 14, design: .rounded))
-                                        Text(product.company)
-                                            .font(.system(size: 14, design: .rounded))
-                                    }
-                                    .font(.system(size: 18, design: .rounded))
-                                    Spacer()
-                                    Text("\(product.price, specifier: "%.2f")")
-                                        .font(.system(size: 20, weight: .bold, design: .rounded))
-                                        .foregroundColor(.accentColor)
-                                }
+                    HStack(alignment: .top) {
+                        VStack(alignment: .leading, spacing: 15) {
+                            Text("Customer Info")
+                                .font(.system(size: 18, weight: .medium, design: .rounded))
+                                .foregroundColor(.accentColor)
+                            Text(order.client.description)
+                                .font(.system(size: 14, design: .rounded))
                                 
-                                
-                            }
                         }
-                        Divider()
+                        Spacer()
+                        VStack(alignment: .leading, spacing: 15) {
+                            Text("Shipping Info")
+                                .font(.system(size: 18, weight: .medium, design: .rounded))
+                                .foregroundColor(.accentColor)
+                            Text(order.shippingAddress.description)
+                                .font(.system(size: 14, design: .rounded))
+                                
+                        }
+                    }
+                    .padding(.bottom, 20)
+                    
+                    HStack(alignment: .bottom) {
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("Status")
+                                .font(.system(size: 18, design: .rounded))
+                                
+                            Text(order.status.rawValue)
+                                .font(.system(size: 20, weight: .bold, design: .rounded))
+                                .foregroundColor(.accentColor)
+                        }
+                        .padding(.bottom, 10)
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            showProductsList.toggle()
+                        }, label: {
+                            Image(systemName: showProductsList ? "chevron.up" : "chevron.down")
+                        })
+                        .padding(.trailing)
+                    }
+                    
+                    if showProductsList {
+                        ForEach(Array(order.shoppingCart.products.keys), id: \.self) { product in
+                            HStack(alignment: .top) {
+                                Image("product_placeholder_image")
+                                    .resizable()
+                                    .frame(width: 150, height: 150)
+                                VStack(alignment: .leading, spacing: 20) {
+                                    Text(product.id)
+                                        .font(.system(size: 14, design: .rounded))
+                                    HStack(alignment: .bottom) {
+                                        VStack(alignment: .leading, spacing: 5) {
+                                            Text(product.name)
+                                                .font(.system(size: 14, design: .rounded))
+                                            Text(product.company)
+                                                .font(.system(size: 14, design: .rounded))
+                                        }
+                                        .font(.system(size: 18, design: .rounded))
+                                        Spacer()
+                                        Text("\(product.price, specifier: "%.2f")")
+                                            .font(.system(size: 20, weight: .bold, design: .rounded))
+                                            .foregroundColor(.accentColor)
+                                    }
+                                    
+                                    
+                                }
+                            }
+                            Divider()
+                        }
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Total Price")
+                            .font(.system(size: 18))
+                            
+                        Text("\(order.totalCost, specifier: "%.2f")")
+                            .font(.system(size: 20, weight: .bold, design: .rounded))
+                            .foregroundColor(.accentColor)
                     }
                 }
-                
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("Total Price")
-                        .font(.system(size: 18))
-                        
-                    Text("\(order.totalCost, specifier: "%.2f")")
-                        .font(.system(size: 20, weight: .bold, design: .rounded))
-                        .foregroundColor(.accentColor)
-                }
-                
+                .padding()
+            }
+            
+            Spacer()
+            
+            VStack(spacing: 20) {
                 Button {
                     withAnimation {
                         profileViewModel.shouldPresentOrderRateView = true
                     }
                 } label: {
-                    Text("Return")
+                    Text("Rate the order")
                         .font(.system(size: 16, weight: .bold, design: .rounded))
                 }
-                .buttonStyle(CustomButton())
+                .buttonStyle(CustomButton(textColor: .accentColor, onlyStroke: true))
                 .frame(width: UIScreen.main.bounds.width * 0.9)
                 .contentShape(Rectangle())
-                .padding(.bottom, 20)
                 
                 Button {
                     withAnimation {
@@ -132,24 +138,24 @@ struct OrderDetailsView: View {
                 .buttonStyle(CustomButton())
                 .frame(width: UIScreen.main.bounds.width * 0.9)
                 .contentShape(Rectangle())
-                .padding(.bottom, 20)
             }
-            .padding()
+            .padding(.bottom, 20)
+            
+            NavigationLink(destination: OrderRateView(order: order)
+                                            .environmentObject(authStateManager)
+                                            .environmentObject(tabBarStateManager)
+                                            .environmentObject(profileViewModel),
+                           isActive: $profileViewModel.shouldPresentOrderRateView) { EmptyView() }
+            
+            NavigationLink(destination: ReturnCreationView(order: order)
+                                            .environmentObject(authStateManager)
+                                            .environmentObject(tabBarStateManager)
+                                            .environmentObject(profileViewModel),
+                           isActive: $profileViewModel.shouldPresentReturnCreationView) { EmptyView() }
+            
         }
         .navigationTitle("Order No. \(order.id)")
         .navigationBarTitleDisplayMode(.inline)
-        
-        NavigationLink(destination: OrderRateView(order: order)
-                                        .environmentObject(authStateManager)
-                                        .environmentObject(tabBarStateManager)
-                                        .environmentObject(profileViewModel),
-                       isActive: $profileViewModel.shouldPresentOrderRateView) { EmptyView() }
-        
-        NavigationLink(destination: ReturnCreationView(order: order)
-                                        .environmentObject(authStateManager)
-                                        .environmentObject(tabBarStateManager)
-                                        .environmentObject(profileViewModel),
-                       isActive: $profileViewModel.shouldPresentReturnCreationView) { EmptyView() }
     }
 }
 
