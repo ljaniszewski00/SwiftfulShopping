649d30a53ed1f949e0424f9e2f6de09363e57bcb
Łukasz Janiszewski <ljaniszewski00@gmail.com>
2022-08-22 18:15:43 +0200

    Creating Custom Date Picker
    To be created:
    - days choosing board
    - implementation of incrementing / decrementing date components methods

---
+++ SwiftfulShopping/Utils/HelperViews/CustomDatePicker.swift
@@ -0,0 +1,20 @@
+//
+//  CustomDatePicker.swift
+//  SwiftfulShopping
+//
+//  Created by Łukasz Janiszewski on 22/08/2022.
+//
+
+import SwiftUI
+
+struct CustomDatePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomDatePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomDatePicker()
+    }
+}
