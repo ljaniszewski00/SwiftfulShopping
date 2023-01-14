f7c1a63a8610023af9be7ba23146e590c376228e
Łukasz Janiszewski <ljaniszewski00@gmail.com>
2023-01-14 12:37:13 +0100

    Added two new FirestoreProductsManager tests

--- SwiftfulShoppingUnitTests/Firebase/FirebaseFirestore/FirestoreProductsManagerTests.swift
+++ SwiftfulShoppingUnitTests/Firebase/FirebaseFirestore/FirestoreProductsManagerTests.swift
@@ -48,6 +48,65 @@ final class FirestoreProductsManagerTests: XCTestCase {
         
         XCTAssertNil(error)
         XCTAssertFalse(products.isEmpty)
+        
+        let appleProducts = products.filter { $0.company == "Apple Inc." }
+        
+        XCTAssertFalse(appleProducts.isEmpty)
+    }
+    
+    func test_FirestoreProductsManager_checkProductsAvailability() {
+        let expectation: XCTestExpectation = XCTestExpectation(description: "Should check if products are available.")
+        
+        let productsToCheck: [String: Int] = ["7vHPvsjhC2N3DszzpejX": 2,
+                                              "LQHU7yJplIXugoPiLucR": 1000]
+        
+        var checkingResult: [String: Bool] = [:]
+        
+        FirestoreProductsManager.checkProductsAvailability(productsIDsWithQuantity: productsToCheck) { [self] result in
+            switch result {
+            case .success(let fetchedCheckingResult):
+                checkingResult = fetchedCheckingResult
+            case .failure(let errorOccured):
+                error = errorOccured
+            }
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 4)
+        
+        XCTAssertNil(error)
+        XCTAssertFalse(checkingResult.isEmpty)
+        
+        guard let firstProductResult = checkingResult["7vHPvsjhC2N3DszzpejX"] else { return XCTFail() }
+        XCTAssertTrue(firstProductResult)
+        guard let secondProductResult = checkingResult["LQHU7yJplIXugoPiLucR"] else { return XCTFail() }
+        XCTAssertTrue(secondProductResult)
+    }
+    
+    func test_FirestoreProductsManager_checkProductAvailability() {
+        let expectation: XCTestExpectation = XCTestExpectation(description: "Should check if product is available.")
+        
+        let productIDToCheck: String = "7vHPvsjhC2N3DszzpejX"
+        
+        var checkingResult: Bool = false
+        
+        FirestoreProductsManager.checkProductAvailability(productID: productIDToCheck,
+                                                          quantity: 2) { [self] result in
+            switch result {
+            case .success(let fetchedCheckingResult):
+                checkingResult = fetchedCheckingResult
+            case .failure(let errorOccured):
+                error = errorOccured
+            }
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 4)
+        
+        XCTAssertNil(error)
+        XCTAssertTrue(checkingResult)
     }
     
     private func setUpProperties() {
